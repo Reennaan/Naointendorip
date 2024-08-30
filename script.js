981f4c2a582333cc7e48836e0e979bdf567e9225
Renan <qwerty142999@gmail.com>
2024-08-30 01:05:35 -0300

    adding-download-page

--- script.js
+++ script.js
@@ -1,17 +1,74 @@
 window.onload = function getLatestGames(){
-    const url = "http://localhost:8080/game"
-    createGameList(url);
+    if(window.location.href === "http://127.0.0.1:5500/"){
+        const url = "http://localhost:8080/game"
+        createGameList(url);
 
-    const input = document.getElementById("searchInput");
-    input.addEventListener("keydown", function(event){
-        if (event.key === "Enter") {
-            event.preventDefault()
-            document.getElementById("searchBtn").click();
-            
-        }
-    });
+        const input = document.getElementById("searchInput");
+        input.addEventListener("keydown", function(event){
+            if (event.key === "Enter") {
+                event.preventDefault()
+                document.getElementById("searchBtn").click();
+                
+            }
+        });
+    }else{
+        loadPage()
+        console.log("passou");
+    }
+
+   
 
 }
+
+ window.addEventListener('popstate', function(event) {
+       window.history.go(-1);
+    });
+
+
+function loadPage(){
+    var gameData = JSON.parse(localStorage.getItem("gameData"));
+    const container = document.querySelector("#container");
+    const row = document.createElement("div");
+    row.className = "d-grid gap-2 col-6 mx-auto";
+    container.appendChild(row)
+    const title = document.createElement("h1");
+    title.textContent = gameData.name;
+    title.className = "col"
+    row.appendChild(title)
+    const img = document.createElement("img");
+    img.src = gameData.img.toString();
+    img.className = "col";
+    row.appendChild(img);
+    const table = document.createElement("div");
+    table.style.backgroundColor = "white";
+    table.innerHTML = gameData.table;
+    table.className = "col"
+    row.appendChild(table);
+    const features = document.createElement("div");
+    features.textContent = gameData.features;
+    features.className = "col";
+    row.appendChild(features);
+    const review = document.createElement("div");
+    review.textContent = gameData.review;
+    review.className = "col";
+    row.appendChild(review);
+    const btnGroup = document.createElement("div");
+    btnGroup.className = "d-grid gap-2 col-6 mx-auto"
+    row.appendChild(btnGroup);
+    console.log(gameData)
+    for(let i = 0; i < gameData.downloadName.length ; i++){
+        const btn = document.createElement("button")
+        btn.className = "btn btn-primary";
+        btn.type = "button";
+        btn.textContent = gameData.downloadName[i];
+        btn.onclick = () => {
+            window.open(gameData.downloadLink[i],'_blank');
+        };
+        btnGroup.appendChild(btn);
+    }
+
+}
+
 
 function createGameList(url){
     var gameList = []
@@ -43,7 +100,7 @@ function createGameList(url){
 }
 
 function refreshPage(){
-    window.location.reload();
+    window.location.href = "http://127.0.0.1:5500/"
 }
 
 
@@ -55,9 +112,10 @@ function createCards(list = []){
 
     list.forEach(game =>{
        const a = document.createElement("a");
-       a.href = game.link
+       
        a.style.marginTop = "1rem";
        a.className = "text-decoration-none"
+       //a.href = game.link
        const card = document.createElement("div");
        card.className = "card"
        card.style.width = "18rem";
@@ -66,7 +124,9 @@ function createCards(list = []){
        const img = document.createElement("img")
        img.src = game.img
        img.className = "card-img-top";
-       
+       img.onclick = function(){
+            downloadPage(game.link)
+        }
        card.appendChild(img);
        const cardBody = document.createElement("div");
        cardBody.className = "card-body";
@@ -95,6 +155,43 @@ function search(game){
     }
     
 }
+
+function downloadPage(link) {
+    console.log(link);
+    const url = "http://localhost:8080/download"
+
+    const encoded = encodeURIComponent(link)
+
+    fetch(url+`?link=${encoded}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Request failed");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const gameData = {
+                name: data.name,
+                img: data.img,
+                table: data.table,
+                features: data.features,
+                review: data.review,
+                downloadName: data.downloadName,
+                downloadLink: data.downloadLink
+            }
+
+           localStorage.setItem('gameData', JSON.stringify(gameData));
+           //console.log(gameData);
+           //console.log(localStorage.getItem('gameData'));
+           window.location.replace('downloadPage.html')
+        })
+
+        
+        .catch(error => {
+            console.error("deu merda:", error);
+        });
+}
+
 
 
 
